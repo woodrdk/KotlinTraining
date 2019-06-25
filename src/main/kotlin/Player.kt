d310c4c54d0ac8596ddae5b6105e05a7b6fa68d9
Robert Wood Jr <rdrwood@gmail.com>
2019-06-24 20:04:56 -0700

    classes for kotlin

---
+++ src/main/kotlin/Player.kt
@@ -0,0 +1,15 @@
+
+class Player( val name: String, var level: Int = 1) {
+    var lives = 3
+    var score = 0
+
+
+    fun show(){
+        println("""
+            name: $name
+            lives: $lives
+            level: $level
+            score: $score
+            """)
+    }
+}
