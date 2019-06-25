dc675a22dff3860da07bcfa58ef0b9b36cf41653
Robert Wood Jr <rdrwood@gmail.com>
2019-06-24 18:14:08 -0700

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,17 @@
+fun main(args: Array<String>) {
+    println("Hello World!")
+    println("My next KOTLIN program.")
+
+    var rob: String = "Rob Wood" // declares a string of Rob Wood
+    println(rob)                 // prints the var rob
+
+    var rob2: String
+    rob2 = "Rob Wood 2"
+
+    println(rob2)
+
+    var income: Int = 82
+    var monthly: Int = income * 4
+    println(income)
+    println(monthly)
+}
