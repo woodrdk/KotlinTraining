68e934aa6987510f515cef3cf4877cb61808d698
Robert Wood Jr <rdrwood@gmail.com>
2019-06-24 19:31:56 -0700

    conditions and var vals

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,33 +1,74 @@
 fun main(args: Array<String>) {
+//    val lives = 3
+//
+//    var isGameOver = ( lives < 1 )
+//    println(isGameOver)
+//    if (isGameOver){
+//        println("Game over!")
+//    }
+//    else{
+//        println("You have $lives lives left")
+//    }
+
+    println("How old are you: ")
+    val age = readLine()!!.toInt()
+    println("Age is $age")
+
+    val message: String
+    message = when{
+        age < 18 -> "You're too young to vote"
+        age == 100 -> "Congratulations"
+        else -> "You can vote"
+    }
+
+//    message = if (age < 18){
+//        "You're too young to vote"
+//    }
+//    else if (age == 100){
+//        "Congratulations"
+//    }
+//    else{
+//        "You can vote"
+//    }
+    println(message)
+
+}
+
+
+fun oldVids(){
     println("Hello World!")
     println("My next KOTLIN program.")
+    val bob = "Dracula"
+    val rob = "Rob Wood" // declares a string of Rob Wood
+    println("My name is $rob")                 // prints the var rob
 
-    var rob: String = "Rob Wood" // declares a string of Rob Wood
-    println(rob)                 // prints the var rob
+    val rob2 = "Rob Wood 2"
 
-    var rob2: String
-    rob2 = "Rob Wood 2"
+    println("My name is $rob2")
+    println("My name is ${rob + " " + rob2}")
 
-    println(rob2)
-    println(rob + " " + rob2)
-
-    var income: Int = 82
-    var monthly: Int = income * 4
-    println(income)
-    println(monthly)
+    val income = 82
+    val monthly = income * 4
+    println("My income is $income")
+    println("My monthly income is $monthly")
 
     println()
 
-    val apples: Int = 6
-    val oranges: Int = 5
-    var fruit: Int = apples + oranges
-    println(fruit)
+    val apples = 6
+    val oranges = 5
+    val fruit: Int = apples + oranges
+    println("I Have $fruit pieces of fruit")
     println(apples / 4)
+    println("A quarter of the apples is ${apples / 4}")
+
 
     println()
-    val weeks: Int = 130
+    val weeks = 130
     val years: Double  = weeks / 52.0
     println("$weeks weeks is $years years")
     println(years)
+
     println("My name is $rob")
+    println("My name is $bob")
+
 }
